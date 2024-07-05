d7443095de42276df956e1e4c2f5d9b282e4a994
kaufman79 <kaufman79@gmail.com>
2024-07-05 12:00:35 -0400

    add credit card functionality

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import pandas
 import pandas as pd
 
 df = pd.read_csv("hotels.csv", dtype={"id": str})
-
+card_df = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -40,15 +41,38 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration,
+                     "holder": holder, "cvc": cvc}
+        if card_data in card_df:
+            return True
+        else:
+            return False
+
+
 if __name__ == "__main__":
     print(df)
     hotel_ID = input("Enter the id of the hotel: ")
     hotel = Hotel(hotel_ID)
     if hotel.available():
-        hotel.book()
-        name = input("Enter your name: ")
-        reservation_ticket = ReservationTicket(customer_name=name,
-                                               hotel_object=hotel)
-        print(reservation_ticket.generate())
+        # if actual app, we would ask user for this info:
+        # card_number = input("Credit card number: ")
+        # card_expire_date = input("Card expiration date: ")
+        # cardholder_name = input("Cardholder name: ")
+        # cvc_code = input("Card CVC code: ")
+        credit_card = CreditCard(number="1234567890123456")
+        if credit_card.validate(expiration="12/26",
+                                 holder="JOHN SMITH", cvc="123"):
+            hotel.book()
+            name = input("Enter your name: ")
+            reservation_ticket = ReservationTicket(customer_name=name,
+                                                   hotel_object=hotel)
+            print(reservation_ticket.generate())
+        else:
+            print("There was a problem with your payment")
     else:
         print("Hotel is not available")
